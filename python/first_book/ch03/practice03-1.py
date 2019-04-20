4097d7fcef1e25a395a3daa9e285bcad5041ddf8
Hsko-Godol <rainbowlinek@gmail.com>
2019-04-20 23:47:20 +0900

    Fin the chapter 3 about the first book of python

---
+++ python/first_book/ch03/practice03-1.py
@@ -0,0 +1,13 @@
+def get_input():
+	return input("첫 번째 입력: ")
+
+def concat_and_print(input_1, input_2):
+	print("두 입력의 합: " + input_1 + input_2)
+
+def main():
+	data_1 = get_input()
+	data_2 = get_input()
+
+	concat_and_print(data_1, data_2)
+
+main()
