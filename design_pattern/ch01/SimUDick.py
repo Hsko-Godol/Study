cbbc598906ac3d7f016cc08e12cf31c99b811d84
Hsko-Godol <rainbowlinek@gmail.com>
2019-05-11 15:44:45 +0900

    Add a simulator

---
+++ design_pattern/ch01/SimUDick.py
@@ -0,0 +1,23 @@
+import MallardDuck, RedheadDuck, RubberDuck
+
+def main():
+    mallard = MallardDuck()
+    mallard.quack()
+    mallard.swim()
+    mallard.display()
+    mallard.fly()
+
+    redhead = RedheadDuck()
+    redhead.quack()
+    redhead.swim()
+    redhead.display()
+    redhead.fly()
+
+    rubber = RubberDuck()
+    rubber.quack()
+    rubber.swim()
+    rubber.display()
+    rubber.fly()
+
+if __name__ == '__main__':
+    main()
